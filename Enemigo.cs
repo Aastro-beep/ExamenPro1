d39c6c2edf9f0342625985c3be066f495c0fc349
Sergio <chechogarcia2020@gmail.com>
2026-09-21 19:44:53 -0500

    jugador y enemigo listos, falta program

---
+++ Enemigo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenProm
+{
+    public class Enemigo
+    {
+        public int HealthEnemy;
+        public int DamageEnemy;
+        public Enemigo(int vida, int dano)
+        {
+            HealthEnemy = vida;
+            HealthEnemy = dano;
+        }
+        public void RecibirDamageEnemy(int cantidad)
+        {
+            HealthEnemy = HealthEnemy - cantidad;
+            if (HealthEnemy < 0)
+            {
+                HealthEnemy = 0;
+            }
+        }
+        public int ObtenerDamageEnemy()
+        {
+            return DamageEnemy;
+        }
+        public bool IsAliveEnemy()
+        {
+            if (HealthEnemy > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Jugador.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenProm
+{
+    public class Jugador
+    {
+        public int Health;
+        public int Damage;
+
+        public Jugador (int vida, int dano)
+        {
+            Health = vida;
+            Damage = dano;
+        }
+        public void RecibirDamage(int cantidad)
+        {
+            Health = Health - cantidad;
+            if (Health < 0)
+            {
+                Health = 0;
+            }
+        }
+        public int ObtenerDamage()
+        {
+            return Damage;
+        }
+        public bool IsAlive()
+        {
+            if (Health > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
